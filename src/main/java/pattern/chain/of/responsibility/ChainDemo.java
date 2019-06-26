5e319856b125562435d9d1ab91b838e91f11bfd5
Efrem Dubrovin <efremdubrovin@gmail.com>
2019-06-27 01:16:10 +0300

    chain of responsibility: client is decoupled from handler Handlers

--- src/main/java/pattern/chain/of/responsibility/ChainDemo.java
+++ src/main/java/pattern/chain/of/responsibility/ChainDemo.java
@@ -4,31 +4,48 @@ import java.util.Random;
 
 public class ChainDemo {
 	public static void main(String[] args) {
-		Handler[] nodes = {new Handler(), new Handler(),
-			new Handler(), new Handler()};
-		for (int i = 1, j; i < 6; i++) {
+		Handler rootChain = new Handler();
+		rootChain.add(new Handler());
+		rootChain.add(new Handler());
+		rootChain.add(new Handler());
+		rootChain.wrapAround(rootChain);
+		for (int i = 1; i < 6; i++) {
 			System.out.println("Operation #" + i + ":");
-			j = 0;
-			while (!nodes[j].execute(i)) {
-				j = (j + 1) % nodes.length;
-			}
+			rootChain.execute(i);
 			System.out.println();
 		}
 	}
 }
 
 class Handler {
-	private static final Random RANDOM = new Random();
+	private final static Random RANDOM = new Random();
 	private static int nextID = 1;
-
 	private int id = nextID++;
-	public boolean execute(int num) {
+
+	private Handler nextInChain;
+
+	public void add(Handler next) {
+		if (nextInChain == null) {
+			nextInChain = next;
+		} else {
+			nextInChain.add(next);
+		}
+	}
+
+	public void wrapAround(Handler root) {
+		if (nextInChain == null) {
+			nextInChain = root;
+		} else {
+			nextInChain.wrapAround(root);
+		}
+	}
+	public void execute(int num) {
 		if (RANDOM.nextInt(4) != 0) {
 			System.out.println("   " + id + "-busy  ");
-			return false;
+			nextInChain.execute(num);
+		} else {
+			System.out.println(id + "-handled-" + num);
 		}
-		System.out.println(id + "-handled-" + num);
-		return true;
 	}
 
 }
